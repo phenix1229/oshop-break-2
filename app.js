02f5255cd0135acf48a0e294da796d676e37507b
phenix1229 <paul.garay@codeimmersives.com>
2020-03-11 21:30:31 -0400

    working

--- app.js
+++ app.js
@@ -11,12 +11,13 @@ const passport = require('passport');
 const methodOverride = require('method-override');
 require('./lib/passport');
 
+const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users/userRoutes');
 const adminRouter = require('./routes/admin/adminRoutes');
 const productRouter = require('./routes/admin/products/productRouter');
 const cartRouter = require('./routes/cart/cartRoutes');
 
-const getAllCategories = require('./route/admin/middleware/getAllCategories');
+const getAllCategories = require('./routes/admin/middleware/getAllCategories');
 const cartTotal = require('./routes/cart/middleware/cartTotal');
 const app = express();
 require('dotenv').config();
@@ -56,6 +57,8 @@ app.use(
     })
   })
 );
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use((req, res, next) => {
   res.locals.user = req.user;
